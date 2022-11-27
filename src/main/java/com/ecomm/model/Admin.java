5233da3f384cad10f32be20d210413270b2d5e42
bhushan8554 <bhushantaru4297@gmail.com>
2022-11-27 11:48:46 +0530

    addind product model

--- src/main/java/com/ecomm/model/Admin.java
+++ src/main/java/com/ecomm/model/Admin.java
@@ -1,5 +1,26 @@
 package com.ecomm.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Admin {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer customerId;
+	private String name;
+	private String mobileNo;
+	private String email;
+	private String password;
+	private Address add;
 }
--- src/main/java/com/ecomm/model/Customer.java
+++ src/main/java/com/ecomm/model/Customer.java
@@ -1,6 +1,9 @@
 package com.ecomm.model;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -14,11 +17,14 @@ import lombok.NoArgsConstructor;
 public class Customer {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer customerId;
 	private String name;
 	private String mobileNo;
 	private String email;
 	private String password;
+	
+	@Embedded
 	private Address add;
 	
 }
