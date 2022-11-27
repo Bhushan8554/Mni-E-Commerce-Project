30e9e337f92cf41d99183e769fadb50b05b6072a
bhushan8554 <bhushantaru4297@gmail.com>
2022-11-27 21:16:36 +0530

    added exception handler

---
+++ src/main/java/com/ecomm/exception/GlobalExceptionHandler.java
@@ -0,0 +1,100 @@
+package com.ecomm.exception;
+
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.support.BeanDefinitionValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(CustomerException.class)
+	public ResponseEntity<ErrorDetails> ecommerseExceptionHandler(CustomerException ee,WebRequest req){
+		
+		ErrorDetails err=new ErrorDetails(LocalDateTime.now(), ee.getMessage(), req.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(err, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@ExceptionHandler(CartException.class)
+	public ResponseEntity<ErrorDetails> ecommerseExceptionHandler(CartException ee,WebRequest req){
+		
+		ErrorDetails err=new ErrorDetails(LocalDateTime.now(), ee.getMessage(), req.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(err, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@ExceptionHandler(OrderException.class)
+	public ResponseEntity<ErrorDetails> ecommerseExceptionHandler(OrderException ee,WebRequest req){
+		
+		ErrorDetails err=new ErrorDetails(LocalDateTime.now(), ee.getMessage(), req.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(err, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@ExceptionHandler(ProductException.class)
+	public ResponseEntity<ErrorDetails> ecommerseExceptionHandler(ProductException ee,WebRequest req){
+		
+		ErrorDetails err=new ErrorDetails(LocalDateTime.now(), ee.getMessage(), req.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(err, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@ExceptionHandler(BeanDefinitionValidationException.class)
+	public ResponseEntity<ErrorDetails> ecommerseExceptionHandler(BeanDefinitionValidationException ee,WebRequest req){
+		
+		ErrorDetails err=new ErrorDetails(LocalDateTime.now(), ee.getMessage(), req.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(err, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<ErrorDetails> ecommerseExceptionHandler(IllegalArgumentException ee,WebRequest req){
+		
+		ErrorDetails err=new ErrorDetails(LocalDateTime.now(), ee.getMessage(), req.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(err, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<ErrorDetails> ecommerseExceptionHandler(NoHandlerFoundException ee,WebRequest req){
+		
+		ErrorDetails err=new ErrorDetails(LocalDateTime.now(), ee.getMessage(), req.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(err, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorDetails> ecommerseExceptionHandler(MethodArgumentNotValidException ee){
+		
+		ErrorDetails err=new ErrorDetails(LocalDateTime.now(),"Validation Error", ee.getBindingResult().getFieldError().getDefaultMessage());
+		
+		return new ResponseEntity<ErrorDetails>(err, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorDetails> ecommerseExceptionHandler(Exception ee,WebRequest req){
+		
+		ErrorDetails err=new ErrorDetails(LocalDateTime.now(), ee.getMessage(), req.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(err, HttpStatus.NOT_FOUND);
+		
+	}
+}
