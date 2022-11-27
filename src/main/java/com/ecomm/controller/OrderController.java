f817ca9933d52106a8d79046c36c3d968d4469f0
bhushan8554 <bhushantaru4297@gmail.com>
2022-11-27 22:35:48 +0530

    added controller for product

---
+++ src/main/java/com/ecomm/controller/OrderController.java
@@ -0,0 +1,62 @@
+package com.ecomm.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecomm.exception.CustomerException;
+import com.ecomm.exception.OrderException;
+import com.ecomm.model.Customer;
+import com.ecomm.model.Order;
+import com.ecomm.model.OrderType;
+import com.ecomm.service.OrderService;
+
+@RestController
+@RequestMapping("order")
+public class OrderController {
+
+	@Autowired
+	OrderService orderService;
+	
+	@PostMapping("/customer")
+	public ResponseEntity<List<Order>> getOrdersByCustomerMapping(@RequestBody Customer customer) throws OrderException{
+		List<Order> list=orderService.getOrdersByCustomer(customer);
+		return new ResponseEntity<List<Order>>(list,HttpStatus.OK);
+	}
+	@GetMapping("/allorders")
+	public ResponseEntity<List<Order>> getAllOrdersMapping() throws OrderException{
+		List<Order> list=orderService.getAllOrders();
+		return new ResponseEntity<List<Order>>(list,HttpStatus.OK);
+	}
+	@PostMapping("/add")
+	public ResponseEntity<Order> addOrderMapping(@RequestParam("Customer_Id")Integer id) throws OrderException, CustomerException{
+		Order o=orderService.addOrder(id);
+		return new ResponseEntity<Order>(o,HttpStatus.OK);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Order> updateOrderMapping(@RequestBody Order order) throws OrderException, CustomerException{
+		Order o=orderService.updateOrder(order);
+		return new ResponseEntity<Order>(o,HttpStatus.OK);
+	}
+	
+	@GetMapping("/orderid")
+	public ResponseEntity<Order> getOrderByIdMapping(@RequestParam("order_id")Integer id) throws OrderException, CustomerException{
+		Order o=orderService.getOrderByID(id);
+		return new ResponseEntity<Order>(o,HttpStatus.OK);
+	}
+	@GetMapping("/type")
+	public ResponseEntity<List<Order>> getAllOrdersByTypeMapping(@RequestBody OrderType orderType) throws OrderException{
+		List<Order> list=orderService.getAllOrderByType(orderType);
+		return new ResponseEntity<List<Order>>(list,HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/ecomm/controller/ProductController.java
@@ -0,0 +1,76 @@
+package com.ecomm.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecomm.model.Category;
+import com.ecomm.model.Product;
+import com.ecomm.service.ProductService;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+	@Autowired
+	ProductService productService;
+	
+	@GetMapping("/byid")
+	public ResponseEntity<Product> getProductByIdMapping(@RequestParam("products_id")Integer id){
+		Product p=productService.getProdectByid(id);
+		return new ResponseEntity<Product>(p,HttpStatus.OK);
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Product> addProductMapping(@RequestBody Product id){
+		Product p=productService.addProduct(id);
+		return new ResponseEntity<Product>(p,HttpStatus.OK);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Product> UpdateProductMapping(@RequestBody Product id){
+		Product p=productService.updateProductdetails(id);
+		return new ResponseEntity<Product>(p,HttpStatus.OK);
+	}
+	
+	@GetMapping("/All")
+	public ResponseEntity<List<Product>> getAllProductMapping(){
+		List<Product> p=productService.getAllProducts();
+		return new ResponseEntity<List<Product>>(p,HttpStatus.OK);
+	}
+	
+	@GetMapping("/price/asc")
+	public ResponseEntity<List<Product>> getAllProductSortedByPriceMapping(){
+		List<Product> p=productService.getAllProductsSortByPrice();
+		return new ResponseEntity<List<Product>>(p,HttpStatus.OK);
+	}
+	
+	@GetMapping("/rating/asc")
+	public ResponseEntity<List<Product>> getAllProductSortedByRatingMapping(){
+		List<Product> p=productService.getAllProductsSortByRating();
+		return new ResponseEntity<List<Product>>(p,HttpStatus.OK);
+	}
+	
+	@GetMapping("/name")
+	public ResponseEntity<List<Product>> getAllProductByNameMapping(@RequestParam("name")String name){
+		List<Product> p=productService.getProdectByName(name);
+		return new ResponseEntity<List<Product>>(p,HttpStatus.OK);
+	}
+	
+	@GetMapping("/category")
+	public ResponseEntity<List<Product>> getAllProductByCategoryMapping(@RequestBody Category category){
+		List<Product> p=productService.getproductByCategory(category);
+		return new ResponseEntity<List<Product>>(p,HttpStatus.OK);
+	}
+	
+	
+}
